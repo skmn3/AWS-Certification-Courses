8aaaf2cb3b8a6280ceac5fc1f0706d99101212ee
sangjin <sangjin170202@naver.com>
2021-09-22 12:46:36 +0900

    재빌드 효율적으로 하기
    app.js에서 코드가 변경되었을 때 모듈을 다시 받는 현상 없애기

--- node-docker-app/app.js
+++ node-docker-app/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 //Application
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello');
 });
 
 app.listen(PORT, () => {
