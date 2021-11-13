028f92332aa7ca7d0be0c6cf767cd735ed91749f
skmn3 <sangjin170202@naver.com>
2021-11-14 02:20:13 +0900

    [EDIT] RELEASE V0.0.4

--- Practice-git-flow/src/models/index.js
+++ Practice-git-flow/src/models/index.js
@@ -9,6 +9,6 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+
 
 module.exports = db;
