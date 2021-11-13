9bd610e5cd28f93d9552020d50a06f52ecf825b2
sangjin <sangjin170202@naver.com>
2021-11-13 14:26:12 +0900

    [CREATE] models

---
+++ Practice-git-flow/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const { sequelize } = require('./models');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
